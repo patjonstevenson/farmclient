1c71160305d429b944cdd06a52cdaabafbeed787
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-05-20 16:02:57 -0700

    added basic initial store with comments detailing what it should look like with data

--- farmclient/src/state/store/index.js
+++ farmclient/src/state/store/index.js
@@ -1,1 +1,55 @@
-export default {}
+export default {
+    // USER INFO
+    user: {
+        id: null,
+        first_name: "",
+        last_name: "",
+        email: ""
+    },
+
+    // FARMS
+    /*
+        Should be an array of farm objects, eg:
+        [
+            {
+                id: null, // farm_id
+                name: "",
+                timezone: "",
+                pumps: [
+                    {
+                        id: null, // pump_id
+                        name: "",
+                        strategy: null // strategy_id,
+                        valves: [
+                            {
+                                id: null, // valve_id
+                                name: ""
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    */
+    farms: [],
+
+    // Strategies
+    /*
+        Should be an array of strategy objects, eg:
+        [
+            {
+                id: null // strategy_id
+                name: "",
+                tactics: [
+                    {
+                        id: null,
+                        time: "",
+                        humidity_high: null,
+                        dryback: null
+                    }
+                ]
+            }
+        ]
+    */
+    strategies: []
+}
