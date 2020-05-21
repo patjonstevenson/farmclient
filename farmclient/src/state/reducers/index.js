06a2f18e45bc91ac3bbf153cd778c056c89de72c
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-05-20 17:13:15 -0700

    added redux and router libraries

---
+++ farmclient/src/state/reducers/index.js
@@ -0,0 +1,16 @@
+// IMPORT ACTION TYPES
+
+import initialStore from "../store";
+
+export default (state = initialStore, action) => {
+    // For logs
+    console.log("\nReducer running.");
+    console.log("Action: ", action.type);
+    console.log("Payload: ", action.payload);
+
+    switch (action.type) {
+        default:
+            return state;
+    }
+
+}
