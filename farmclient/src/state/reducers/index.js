0148674d89a90a52184fdd699b56fd0a9730765e
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-05-22 15:31:50 -0700

    added import and some basic notes and scaffolding for multiple reducers in reducers/index.js

--- farmclient/src/state/reducers/index.js
+++ farmclient/src/state/reducers/index.js
@@ -1,16 +1,30 @@
+import { combineReducers } from "redux";
 // IMPORT ACTION TYPES
 
 import initialStore from "../store";
 
-export default (state = initialStore, action) => {
+export const reducerTemplate = (state = initialStore, action) => {
     // For logs
     console.log("\nReducer running.");
     console.log("Action: ", action.type);
     console.log("Payload: ", action.payload);
 
     switch (action.type) {
+
+
         default:
             return state;
     }
 
 }
+
+// IMPORT REDUCERS
+// import userReducer from "./userReducer";
+// import farmReducer from "./farmReducer";
+// import strategyReducer from "./strategiesReducer";
+
+export default combineReducers(
+    // userReducer,
+    // farmReducer,
+    // strategyReducer
+);
