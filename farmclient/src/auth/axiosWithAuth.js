3c5ebdcd982de84892c55b0576fe1350b496d899
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-05-22 16:22:01 -0700

    created auth/ and auth/axiosWithAuth.js

---
+++ farmclient/src/auth/axiosWithAuth.js
@@ -0,0 +1,18 @@
+
+import axios from 'axios';
+
+export const axiosWithAuth = () => {
+    const token = localStorage.getItem('token');
+
+    if (token === "undefined") {
+        alert("WARNING. Making requests that might require auth while not logged in.");
+    }
+
+    return axios.create({
+        baseURL: "http://localhost:9000/api/",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `${token}`,
+        }
+    });
+};
--- farmclient/src/state/actions/farm/functions.js
+++ farmclient/src/state/actions/farm/functions.js
@@ -59,7 +59,7 @@ export default {
 
     // POST
     postFarm: (farm) => dispatch => {
-
+        axios
     },
     postPump: (pump) => dispatch => { },
     postValve: (valve) => dispatch => { },
