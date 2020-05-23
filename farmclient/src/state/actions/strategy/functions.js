30a46467841784a159e682f402cf387bcbc09b8a
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-05-22 17:20:00 -0700

    finished writing action functions for strategy

--- farmclient/src/state/actions/strategy/functions.js
+++ farmclient/src/state/actions/strategy/functions.js
@@ -9,12 +9,12 @@ import {
     FETCH_TACTIC_FAILURE,
 
     // POST
-    POST_STRATEGY_START,
-    POST_STRATEGY_SUCCESS,
-    POST_STRATEGY_FAILURE,
-    POST_TACTIC_START,
-    POST_TACTIC_SUCCESS,
-    POST_TACTIC_FAILURE,
+    ADD_STRATEGY_START,
+    ADD_STRATEGY_SUCCESS,
+    ADD_STRATEGY_FAILURE,
+    ADD_TACTIC_START,
+    ADD_TACTIC_SUCCESS,
+    ADD_TACTIC_FAILURE,
 
     // PUT
     UPDATE_STRATEGY_START,
@@ -46,14 +46,111 @@ export default {
     // JUST GET THEM ALL TOGETHER FOR NOW IN USER FUNCTIONS
 
     // POST
-    postStrategy: (strategy) => dispatch => { },
-    postTactic: (tactic) => dispatch => { },
+    addStrategy: (strategy) => async dispatch => {
+        dispatch({ type: ADD_STRATEGY_START });
+        try {
+            const newStrategy = await axiosWithAuth.post("strategies", strategy);
+            dispatch({
+                type: ADD_STRATEGY_SUCCESS,
+                payload: newStrategy
+            });
+        } catch (error) {
+            dispatch({
+                type: ADD_STRATEGY_FAILURE,
+                payload: error
+            });
+        }
+    },
+    addTactic: (tactic, strategy_id) => async dispatch => {
+        dispatch({ type: ADD_TACTIC_START });
+        try {
+            const newTactic = await axiosWithAuth.post(
+                `strategies/${strategy_id}/tactics`,
+                tactic
+            );
+            dispatch({
+                type: ADD_TACTIC_SUCCESS,
+                payload: newTactic
+            });
+        } catch (error) {
+            dispatch({
+                type: ADD_TACTIC_FAILURE,
+                payload: error
+            });
+        }
+    },
 
     // PUT
-    putStrategy: (strategy) => dispatch => { },
-    putTactic: (tactic) => dispatch => { },
+    updateStrategy: (changes, strategy_id) => async dispatch => {
+        dispatch({ type: UPDATE_STRATEGY_START });
+        try {
+            const updated = await axiosWithAuth.put(
+                `strategies/${strategy_id}`,
+                changes
+            );
+            dispatch({
+                type: UPDATE_STRATEGY_SUCCESS,
+                payload: updated
+            });
+        } catch (error) {
+            dispatch({
+                type: UPDATE_STRATEGY_FAILURE,
+                payload: error
+            });
+        }
+    },
+    updateTactic: (changes, strategy_id, tactic_id) => async dispatch => {
+        dispatch({ type: UPDATE_TACTIC_START });
+        try {
+            const updated = await axiosWithAuth.post(
+                `strategies/${strategy_id}/tactics/${tactic_id}`,
+                changes
+            );
+            dispatch({
+                type: UPDATE_TACTIC_SUCCESS,
+                payload: updated
+            });
+        } catch (error) {
+            dispatch({
+                type: UPDATE_TACTIC_FAILURE,
+                payload: error
+            });
+        }
+    },
 
     // DELETE
-    putStrategy: (strategy) => dispatch => { },
-    putTactic: (tactic) => dispatch => { },
+    deleteStrategy: (strategy_id) => async dispatch => {
+        dispatch({ type: DELETE_STRATEGY_START });
+        try {
+            const deleted_id = await axiosWithAuth.delete(
+                `strategies/${strategy_id}`
+            );
+            dispatch({
+                type: DELETE_STRATEGY_SUCCESS,
+                payload: deleted_id
+            });
+        } catch (error) {
+            dispatch({
+                type: DELETE_STRATEGY_FAILURE,
+                payload: error
+            });
+        }
+    },
+    deleteTactic: (strategy_id, tactic_id) => async dispatch => {
+        dispatch({ type: DELETE_TACTIC_START });
+        try {
+            const deleted_id = await axiosWithAuth.delete(
+                `strategies/${strategy_id}/tactics/${tactic_id}`
+            );
+            dispatch({
+                type: DELETE_TACTIC_SUCCESS,
+                payload: deleted_id
+            });
+        } catch (error) {
+            dispatch({
+                type: DELETE_TACTIC_FAILURE,
+                payload: error
+            });
+        }
+    },
 }
