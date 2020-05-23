3fab289ea9fd28e83b2a87e56f0834507c1a9b25
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-05-22 17:40:22 -0700

    finished writing user action functions. all action functions written except for fetching functions

--- farmclient/src/state/actions/user/functions.js
+++ farmclient/src/state/actions/user/functions.js
@@ -1,4 +1,5 @@
 import axiosWithAuth from "../../../requests/axiosWithAuth";
+import axios from "../../../requests/axios";
 import {
     REGISTER_USER_START,
     REGISTER_USER_SUCCESS,
@@ -15,18 +16,54 @@ export default {
     registerUser: user => dispatch => {
         dispatch({ type: REGISTER_USER_START });
         try {
-            const user_id = await axios.post("")
-            dispatch({ type: REGISTER_USER_SUCCESS });
+            const id = await axios.post(
+                "auth/register",
+                user
+            );
+            dispatch({
+                type: REGISTER_USER_SUCCESS,
+                payload: id
+            });
         } catch (error) {
-            dispatch({ type: REGISTER_USER_FAILURE });
+            dispatch({
+                type: REGISTER_USER_FAILURE,
+                payload: error
+            });
         }
     },
     login: credentials => dispatch => {
         dispatch({ type: LOGIN_START });
+        try {
+            const { token, id } = await axios.post(
+                "auth/login",
+                credentials
+            );
+            localStorage.setItem("token", token);
+            dispatch({
+                type: REGISTER_USER_SUCCESS,
+                payload: id
+            });
+        } catch (error) {
+            dispatch({
+                type: REGISTER_USER_FAILURE,
+                payload: error
+            });
+        }
 
     },
     fetchUserData: id => dispatch => {
         dispatch({ type: FETCH_USER_DATA_START });
-
+        try {
+            const userInfo = await axiosWithAuth.get(`users/${id}`);
+            dispatch({
+                type: FETCH_USER_DATA_SUCCESS,
+                payload: userInfo
+            });
+        } catch (error) {
+            dispatch({
+                type: FETCH_USER_DATA_FAILURE,
+                payload: error
+            });
+        }
     }
 }
