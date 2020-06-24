e5dd06838a10a62a66462fd571e2887ba3bd65dd
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-06-24 15:57:13 -0700

    fixed a couple small errors

--- farmclient/src/state/reducer.js
+++ farmclient/src/state/reducer.js
@@ -1,8 +1,8 @@
 import { combineReducers } from "redux";
 
-import userReducer from "../resources/user/reducer";
-import farmReducer from "../resources/farm/reducer";
-import strategyReducer from "../resources/strategy/reducer";
+import userReducer from "./resources/user/reducer";
+import farmReducer from "./resources/farm/reducer";
+import strategyReducer from "./resources/strategy/reducer";
 
 export default combineReducers(
     userReducer,
--- farmclient/src/state/resources/user/action-types.js
+++ farmclient/src/state/resources/user/action-types.js
@@ -6,7 +6,7 @@ export const LOGIN_START = "LOGIN_START";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAILURE = "LOGIN_FAILURE";
 
-export const LOGIN_START = "LOGOUT_START";
+export const LOGOUT_START = "LOGOUT_START";
 export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
 export const LOGOUT_FAILURE = "LOGOUT_FAILURE";
 
