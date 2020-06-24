3d00f187c6dab1acf0da77e07ef606cd11195449
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-06-24 14:10:58 -0700

    added imports to valve reducer

--- farmclient/src/state/resources/valve/reducer.js
+++ farmclient/src/state/resources/valve/reducer.js
@@ -1,4 +1,20 @@
 import initialStore from "../store";
+import {
+    // ADD
+    ADD_VALVE_START,
+    ADD_VALVE_SUCCESS,
+    ADD_VALVE_FAILURE,
+
+    // PUT
+    UPDATE_VALVE_START,
+    UPDATE_VALVE_SUCCESS,
+    UPDATE_VALVE_FAILURE,
+
+    // DELETE
+    DELETE_VALVE_START,
+    DELETE_VALVE_SUCCESS,
+    DELETE_VALVE_FAILURE
+} from "./action-types";
 
 export default (state = initialStore, action) => {
     // For logs
@@ -8,6 +24,7 @@ export default (state = initialStore, action) => {
 
     switch (action.type) {
 
+        
 
         default:
             return state;
