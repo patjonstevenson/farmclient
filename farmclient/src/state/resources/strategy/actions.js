ada1ed164d725f3ecddc43458f1c648adb77ebb3
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-07-02 19:07:46 -0700

    started setting up strategy and tactic adder configs in the registry; changed action exports for strategy and tactic

--- farmclient/src/state/resources/strategy/actions.js
+++ farmclient/src/state/resources/strategy/actions.js
@@ -20,72 +20,72 @@ import {
     DELETE_STRATEGY_SUCCESS,
     DELETE_STRATEGY_FAILURE,
 } from "./action-types";
+import axiosWithAuth from "../../../requests/axiosWithAuth";
 
 // Can then import Strategy from "......strategy/functions";
 // and do Strategy.fetchStrategies() etc
 
-export default {
-    // GET
-    // fetchStrategys: () => {},
-    // fetchStrategyBy
 
-    // ^^^ DO WE EVEN NEED SEPARATE GETs?
-    // OR WILL WE ALWAYS JUST GET THEM WITH THE REST OF USER DATA, IN BULK?
-    // JUST GET THEM ALL TOGETHER FOR NOW IN USER FUNCTIONS
+// GET
+// fetchStrategys: () => {},
+// fetchStrategyBy
 
-    // POST
-    addStrategy: (strategy) => async dispatch => {
-        dispatch({ type: ADD_STRATEGY_START });
-        try {
-            const newStrategy = await axiosWithAuth().post("strategies", strategy);
-            dispatch({
-                type: ADD_STRATEGY_SUCCESS,
-                payload: newStrategy
-            });
-        } catch (error) {
-            dispatch({
-                type: ADD_STRATEGY_FAILURE,
-                payload: error
-            });
-        }
-    },
+// ^^^ DO WE EVEN NEED SEPARATE GETs?
+// OR WILL WE ALWAYS JUST GET THEM WITH THE REST OF USER DATA, IN BULK?
+// JUST GET THEM ALL TOGETHER FOR NOW IN USER FUNCTIONS
 
-    // PUT
-    updateStrategy: (changes, strategy_id) => async dispatch => {
-        dispatch({ type: UPDATE_STRATEGY_START });
-        try {
-            const updated = await axiosWithAuth().put(
-                `strategies/${strategy_id}`,
-                changes
-            );
-            dispatch({
-                type: UPDATE_STRATEGY_SUCCESS,
-                payload: updated
-            });
-        } catch (error) {
-            dispatch({
-                type: UPDATE_STRATEGY_FAILURE,
-                payload: error
-            });
-        }
-    },
+// POST
+export const addStrategy = (strategy) => async dispatch => {
+    dispatch({ type: ADD_STRATEGY_START });
+    try {
+        const newStrategy = await axiosWithAuth().post("strategies", strategy);
+        dispatch({
+            type: ADD_STRATEGY_SUCCESS,
+            payload: newStrategy
+        });
+    } catch (error) {
+        dispatch({
+            type: ADD_STRATEGY_FAILURE,
+            payload: error
+        });
+    }
+};
 
-    // DELETE
-    deleteStrategy: (strategy_id) => async dispatch => {
-        dispatch({ type: DELETE_STRATEGY_START });
-        try {
-            const deleted_id = await axiosWithAuth().delete(
-                `strategies/${strategy_id}`
-            );
-            dispatch({
-                type: DELETE_STRATEGY_SUCCESS,
-                payload: deleted_id
-            });
-        } catch (error) {
-            dispatch({
-                type: DELETE_STRATEGY_FAILURE,
-                payload: error
-            });
-        }
-    },
+// PUT
+export const updateStrategy = (changes, strategy_id) => async dispatch => {
+    dispatch({ type: UPDATE_STRATEGY_START });
+    try {
+        const updated = await axiosWithAuth().put(
+            `strategies/${strategy_id}`,
+            changes
+        );
+        dispatch({
+            type: UPDATE_STRATEGY_SUCCESS,
+            payload: updated
+        });
+    } catch (error) {
+        dispatch({
+            type: UPDATE_STRATEGY_FAILURE,
+            payload: error
+        });
+    }
+}
+
+// DELETE
+export const deleteStrategy = (strategy_id) => async dispatch => {
+    dispatch({ type: DELETE_STRATEGY_START });
+    try {
+        const deleted_id = await axiosWithAuth().delete(
+            `strategies/${strategy_id}`
+        );
+        dispatch({
+            type: DELETE_STRATEGY_SUCCESS,
+            payload: deleted_id
+        });
+    } catch (error) {
+        dispatch({
+            type: DELETE_STRATEGY_FAILURE,
+            payload: error
+        });
+    }
 }
--- farmclient/src/state/resources/tactic/actions.js
+++ farmclient/src/state/resources/tactic/actions.js
@@ -20,75 +20,74 @@ import {
     DELETE_TACTIC_SUCCESS,
     DELETE_TACTIC_FAILURE
 } from "./action-types";
-
+import axiosWithAuth from "../../../requests/axiosWithAuth";
 // Can then import Strategy from "......strategy/functions";
 // and do Strategy.fetchStrategies() etc
 
-export default {
-    // GET
-    // fetchStrategys: () => {},
-    // fetchStrategyBy
 
-    // ^^^ DO WE EVEN NEED SEPARATE GETs?
-    // OR WILL WE ALWAYS JUST GET THEM WITH THE REST OF USER DATA, IN BULK?
-    // JUST GET THEM ALL TOGETHER FOR NOW IN USER FUNCTIONS
+// GET
+// fetchStrategys: () => {},
+// fetchStrategyBy
 
-    // POST
-    addTactic: (tactic, strategy_id) => async dispatch => {
-        dispatch({ type: ADD_TACTIC_START });
-        try {
-            const newTactic = await axiosWithAuth().post(
-                `tactics`,
-                tactic
-            );
-            dispatch({
-                type: ADD_TACTIC_SUCCESS,
-                payload: newTactic
-            });
-        } catch (error) {
-            dispatch({
-                type: ADD_TACTIC_FAILURE,
-                payload: error
-            });
-        }
-    },
+// ^^^ DO WE EVEN NEED SEPARATE GETs?
+// OR WILL WE ALWAYS JUST GET THEM WITH THE REST OF USER DATA, IN BULK?
+// JUST GET THEM ALL TOGETHER FOR NOW IN USER FUNCTIONS
 
-    // PUT
-    updateTactic: (changes, strategy_id, tactic_id) => async dispatch => {
-        dispatch({ type: UPDATE_TACTIC_START });
-        try {
-            const updated = await axiosWithAuth().post(
-                `tactics/${tactic_id}`,
-                changes
-            );
-            dispatch({
-                type: UPDATE_TACTIC_SUCCESS,
-                payload: updated
-            });
-        } catch (error) {
-            dispatch({
-                type: UPDATE_TACTIC_FAILURE,
-                payload: error
-            });
-        }
-    },
+// POST
+export const addTactic = (tactic, strategy_id) => async dispatch => {
+    dispatch({ type: ADD_TACTIC_START });
+    try {
+        const newTactic = await axiosWithAuth().post(
+            `tactics`,
+            tactic
+        );
+        dispatch({
+            type: ADD_TACTIC_SUCCESS,
+            payload: newTactic
+        });
+    } catch (error) {
+        dispatch({
+            type: ADD_TACTIC_FAILURE,
+            payload: error
+        });
+    }
+};
 
-    // DELETE
-    deleteTactic: (strategy_id, tactic_id) => async dispatch => {
-        dispatch({ type: DELETE_TACTIC_START });
-        try {
-            const deleted_id = await axiosWithAuth().delete(
-                `tactics/${tactic_id}`
-            );
-            dispatch({
-                type: DELETE_TACTIC_SUCCESS,
-                payload: deleted_id
-            });
-        } catch (error) {
-            dispatch({
-                type: DELETE_TACTIC_FAILURE,
-                payload: error
-            });
-        }
-    },
-}
+// PUT
+export const updateTactic = (changes, strategy_id, tactic_id) => async dispatch => {
+    dispatch({ type: UPDATE_TACTIC_START });
+    try {
+        const updated = await axiosWithAuth().post(
+            `tactics/${tactic_id}`,
+            changes
+        );
+        dispatch({
+            type: UPDATE_TACTIC_SUCCESS,
+            payload: updated
+        });
+    } catch (error) {
+        dispatch({
+            type: UPDATE_TACTIC_FAILURE,
+            payload: error
+        });
+    }
+};
+
+// DELETE
+export const deleteTactic = (strategy_id, tactic_id) => async dispatch => {
+    dispatch({ type: DELETE_TACTIC_START });
+    try {
+        const deleted_id = await axiosWithAuth().delete(
+            `tactics/${tactic_id}`
+        );
+        dispatch({
+            type: DELETE_TACTIC_SUCCESS,
+            payload: deleted_id
+        });
+    } catch (error) {
+        dispatch({
+            type: DELETE_TACTIC_FAILURE,
+            payload: error
+        });
+    }
+};
